bd6c7d88dc8ffa1a9c790a062cbca08c4327dcde
Jon Knight <jon@babble.io>
2019-07-08 15:48:23 +0100

    Fix typo in constants file

--- src/common/constants.go
+++ src/common/constants.go
@@ -50,14 +50,14 @@ const (
 	PeersGenesisJSONTarget = "babble/peers.genesis.json"
 	GenesisJSONTarget      = "eth/genesis.json"
 
-//TODO Change this value when monetd repo is public	
-	DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/evm-lite/poa/e2e/smart-contracts/monet.sol
-//	DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/monetd/master/smart-contract/genesis.sol"	
-	TemplateContract        = "template.sol"
-	GenesisContract         = "contract0.sol"
-	GenesisABI              = "contract0.abi"
-	DefaultAccountBalance   = "1234000000000000000000"
-	DefaultContractAddress  = "abbaabbaabbaabbaabbaabbaabbaabbaabbaabba"
+	//TODO Change this value when monetd repo is public
+	DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/evm-lite/poa/e2e/smart-contracts/monet.sol"
+	//	DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/monetd/master/smart-contract/genesis.sol"
+	TemplateContract       = "template.sol"
+	GenesisContract        = "contract0.sol"
+	GenesisABI             = "contract0.abi"
+	DefaultAccountBalance  = "1234000000000000000000"
+	DefaultContractAddress = "abbaabbaabbaabbaabbaabbaabbaabbaabbaabba"
 )
 
 func init() {
