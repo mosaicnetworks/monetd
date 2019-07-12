facf565c59709a04937aab1255b0f71c64665b77
Jon Knight <jon@babble.io>
2019-07-12 11:51:22 +0100

    Now the repo is public, change the default smart contract to a URL inside this repo.

--- src/common/constants.go
+++ src/common/constants.go
@@ -52,13 +52,13 @@ const (
 	GenesisJSONTarget      = "eth/genesis.json"
 
 	//TODO Change this value when monetd repo is public
-	DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/evm-lite/poa/e2e/smart-contracts/monet.sol"
-	//	DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/monetd/master/smart-contract/genesis.sol"
-	TemplateContract       = "template.sol"
-	GenesisContract        = "contract0.sol"
-	GenesisABI             = "contract0.abi"
-	DefaultAccountBalance  = "1234000000000000000000"
-	DefaultContractAddress = "abbaabbaabbaabbaabbaabbaabbaabbaabbaabba"
+	//  DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/evm-lite/poa/e2e/smart-contracts/monet.sol"
+	DefaultSolidityContract = "https://raw.githubusercontent.com/mosaicnetworks/monetd/master/smart-contract/genesis.sol"
+	TemplateContract        = "template.sol"
+	GenesisContract         = "contract0.sol"
+	GenesisABI              = "contract0.abi"
+	DefaultAccountBalance   = "1234000000000000000000"
+	DefaultContractAddress  = "abbaabbaabbaabbaabbaabbaabbaabbaabbaabba"
 )
 
 func init() {
