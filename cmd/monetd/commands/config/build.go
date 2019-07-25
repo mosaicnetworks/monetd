8a2e395a69c2c156195b8156f520c1b90b45633a
Jon Knight <jon@babble.io>
2019-07-25 10:25:07 +0100

    Add info messages to build and pull. With recent changes to use default values for addresses and nodes for these commands, the actual default values are not actually shown.
    By adding the information message, monetd
    is clear what it has done. Hopefully this will
    reduce the support requirements to explain
    what is happening.

--- cmd/monetd/commands/config/build.go
+++ cmd/monetd/commands/config/build.go
@@ -3,6 +3,8 @@ package config
 import (
 	"fmt"
 
+	"github.com/mosaicnetworks/monetd/src/poa/common"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -21,6 +23,9 @@ with monetd. It uses one of the accounts from the keystore to define a network
 consisting of a unique node, which is automatically added to the PoA whitelist.
 Additionally, all the accounts in [datadir]/keystore are credited with a large
 amount of tokens in the genesis file. This command is mostly used for testing.
+
+If the --address flag is omitted, the first non-loopback address for this 
+instance is used.
 `,
 		Args: cobra.ExactArgs(1),
 		RunE: buildConfig,
@@ -39,6 +44,8 @@ func addBuildFlags(cmd *cobra.Command) {
 
 func buildConfig(cmd *cobra.Command, args []string) error {
 	moniker := args[0]
+
+	common.InfoMessage(fmt.Sprintf("Builing configuration for key %s on %s", moniker, addressParam))
 
 	err := pconfig.BuildConfig(configuration.Configuration.DataDir, moniker, addressParam, passwordFile)
 	if err != nil {
