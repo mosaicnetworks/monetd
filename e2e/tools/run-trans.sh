4cfe4b28531b3c956ced7ec1648c7dba36bd5760
Jon Knight <jon@babble.io>
2019-09-13 11:50:01 +0100

    Batch process to enable keepalive in curl

--- e2e/tools/run-trans.sh
+++ e2e/tools/run-trans.sh
@@ -2,11 +2,32 @@
 
 
 URIFILE=$1
+METHOD="batch"
+# TMPFILE=/tmp/tmp.curl.$$
 
 
-grep http $URIFILE | {
-while read host post  
-do
-   curl $host -d "$post" -X POST
-done
-}
+
+if [ "$METHOD" == "batch"] ; then
+
+    cmd="curl"
+
+    grep http $URIFILE | {
+    while read host post  
+    do
+    if [ "$cmd" != "curl" ] ; then 
+        cmd="$cmd --next"
+    fi 
+    cmd="$cmd $host -d $post -X POST"
+    echo $cmd
+    done
+    $cmd
+    } 
+else
+    grep http $URIFILE | {
+    while read host post  
+    do
+    curl $host -d "$post" -X POST
+    done
+    }
+fi
+
