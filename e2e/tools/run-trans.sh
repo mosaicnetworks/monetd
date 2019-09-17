3d4ea950517773bbdfe10abeb874555451907443
Jon Knight <jon@babble.io>
2019-09-17 11:26:35 +0100

    Better error handling in run-trans.sh
    There are now .out and .err output files created in /tmp/trans.$$ for
    the stdout and stderr output. In normal opertation there is just a dot
    per transaction.

--- e2e/tools/run-trans.sh
+++ e2e/tools/run-trans.sh
@@ -2,10 +2,16 @@
 
 
 URIFILE=$1
-METHOD="batch"
+# METHOD="batch"
+METHOD="notbatch"
 # TMPFILE=/tmp/tmp.curl.$$
 
+OUTFILE=$URIFILE.out
+ERRFILE=$URIFILE.err
+TMPFILE=$URIFILE.tmp
 
+> $OUTFILE
+> $ERRFILE
 
 if [ "$METHOD" == "batch" ] ; then
 
@@ -26,8 +32,45 @@ else
     grep http $URIFILE | {
     while read host post  
     do
-    curl -s $host -d "$post" -X POST
+        
+         json=$( curl -v $host -d "$post" -X POST  2> $TMPFILE); 
+
+         rc="$(grep -v "POST is already" $TMPFILE)"       
+        
+
+   
+
+         if [ "$(echo $rc | grep -c "HTTP/1.1 200 OK")" -gt 0 ] ; then
+            echo -n "."
+         else   
+          echo "$host $post" >> $ERRFILE
+          echo "$rc" >> $ERRFILE
+          echo "$json\n\n" >> $ERRFILE           
+          echo "" >> $ERRFILE   
+
+          echo ""
+          echo "ERROR: $URIFILE"
+          echo "$host $post" 
+          echo ""
+
+          echo "$rc"           
+          echo ""
+
+          echo "$json"
+          echo ""               
+         fi   
+
+         echo "$host $post" >> $OUTFILE
+         echo "$rc" >> $OUTFILE
+         echo "$json" >> $OUTFILE
+         echo "" >> $OUTFILE
     done
     }
 fi
 
+
+if [ ! -s $ERRFILE ] ; then
+    rm  $ERRFILE
+fi
+
+rm $TMPFILE
