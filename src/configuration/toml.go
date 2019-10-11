206af45e25b1d661da68c3b178175ce3567698e2
Jon Knight <jon@babble.io>
2019-10-11 15:03:30 +0100

    Added comments to the monetd.toml template

--- src/configuration/toml.go
+++ src/configuration/toml.go
@@ -7,21 +7,35 @@ import (
 )
 
 const configTOML = `
+# Set to true for extended logging
 verbose = "{{.Verbose}}"
+# The IP:PORT of the HTTP API service (defaults to :8080)
 api-listen = "{{.APIAddr}}"
 
 [babble]
+# Advertise IP:PORT of Babble node. This is shared in the peers.json
+# file and should not be an internal IP
   listen = "{{.Babble.BindAddr}}"
+# The heartbeat timer, the time in milliseconds between gossips  
   heartbeat = "{{.Babble.Heartbeat}}"
+# TCP timeout  
   timeout = "{{.Babble.TCPTimeout}}"
+# Number of items in the LRU cache  
   cache-size = {{.Babble.CacheSize}}
+# Max number of events per sync  
   sync-limit = {{.Babble.SyncLimit}}
+# Max number of pool connections  
   max-pool = {{.Babble.MaxPool}}
+# Bootstrap Babble from database  
   bootstrap = {{.Babble.Bootstrap}}
+# Moniker for this node  
   moniker = "{{.Babble.Moniker}}"
 
 [eth]
+# megabytes of memory allocated to internal caching 
+#    (min 16MB / database forced) (default 128)
   cache = {{.Eth.Cache}}
+# minimum gasprice of transactions submitted through this node (eg 1T) (default "0")  
   min-gas-price = {{.Eth.MinGasPrice}}
 `
 
