b5f488aa9c5a149c44e4e3d48f696fa9b6e4cf1d
Jon Knight <jon@babble.io>
2019-10-14 19:35:53 +0100

    Amend the monetd config contract command to have multuple initial validators either in the form of a moniker or a moniker=address format.

--- cmd/monetd/commands/config/contract.go
+++ cmd/monetd/commands/config/contract.go
@@ -2,13 +2,11 @@ package config
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
 
-	"github.com/mosaicnetworks/babble/src/peers"
 	"github.com/mosaicnetworks/monetd/src/configuration"
 	"github.com/mosaicnetworks/monetd/src/contract"
 	"github.com/mosaicnetworks/monetd/src/crypto"
@@ -19,13 +17,15 @@ import (
 // newContractCommand returns the ContractCmd
 func newContractCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "contract [moniker]",
+		Use:   "contract [validator] [validator] ...",
 		Short: "display poa contract",
 		Long: `
 Display the PoA smart contract.
 
-Outputs the standard monetd contract, configured with [moniker] in the initial
-whitelist.
+Outputs the standard monetd contract, configured with the list of validators
+in the initial whitelist. The validator arguments are either a moniker that
+is in the keystore, or in the form moniker=address. The two forms can be mixed
+in the same command line.
 `,
 		Args: cobra.MinimumNArgs(1),
 		RunE: contractConfig,
@@ -35,42 +35,32 @@ whitelist.
 
 func contractConfig(cmd *cobra.Command, args []string) error {
 	var err error
-	var solSource string
-	node := args[0]
 
-	if !strings.Contains(node, "=") {
-		jsonfile := filepath.Join(configuration.DefaultKeystoreDir(), node+".json")
+	var minPeers []*contract.MinimalPeerRecord
 
-		// Read key from file.
-		keyjson, err := ioutil.ReadFile(jsonfile)
-		if err != nil {
-			return fmt.Errorf("Failed to read the keyfile at '%s': %v", jsonfile, err)
-		}
-
-		k := new(crypto.EncryptedKeyJSONMonet)
-		if err := json.Unmarshal(keyjson, k); err != nil {
-			return err
-		}
-
-		whitelistPeers := []*peers.Peer{
-			peers.NewPeer(k.PublicKey, "localhost:1337", node),
-		}
-
-		solSource, err = contract.GetFinalSoliditySource(whitelistPeers)
-	} else {
-		var minPeers []*contract.MinimalPeerRecord
-
-		for _, peer := range args {
-			splitRec := strings.Split(peer, "=")
-			if len(splitRec) != 2 {
-				return errors.New("malformed parameter " + peer)
-			}
+	for _, peer := range args {
+		splitRec := strings.Split(peer, "=")
+		if len(splitRec) > 1 {
 			minPeers = append(minPeers, &contract.MinimalPeerRecord{Address: splitRec[1], Moniker: splitRec[0]})
+		} else {
+			jsonfile := filepath.Join(configuration.DefaultKeystoreDir(), peer+".json")
+
+			// Read key from file.
+			keyjson, err := ioutil.ReadFile(jsonfile)
+			if err != nil {
+				return fmt.Errorf("Failed to read the keyfile at '%s': %v", jsonfile, err)
+			}
+
+			k := new(crypto.EncryptedKeyJSONMonet)
+			if err := json.Unmarshal(keyjson, k); err != nil {
+				return err
+			}
+			minPeers = append(minPeers, &contract.MinimalPeerRecord{Address: k.Address, Moniker: peer})
 		}
-
-		solSource, err = contract.GetFinalSoliditySourceFromAddress(minPeers)
-
 	}
+
+	solSource, err := contract.GetFinalSoliditySourceFromAddress(minPeers)
+
 	fmt.Print(solSource)
 	fmt.Println("")
 	return err
