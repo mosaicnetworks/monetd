913c8f9cfc4642f0b23bb8f63abf723cb9e7a17e
Jon Knight <jon@babble.io>
2019-10-14 12:32:09 +0100

    Minor refactor of CheckIP and added a test for CheckIP function

--- src/common/utils.go
+++ src/common/utils.go
@@ -52,13 +52,14 @@ func CheckIP(ip string, portOnlyOk bool) bool {
 	IP := net.ParseIP(trimmedIP)
 	if IP == nil {
 		return true
-	} else {
-		_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
-		_, private24BitBlock2, _ := net.ParseCIDR("127.0.0.0/8")
-		_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
-		_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
-		private = private24BitBlock2.Contains(IP) || private24BitBlock.Contains(IP) || private20BitBlock.Contains(IP) || private16BitBlock.Contains(IP)
 	}
+
+	_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
+	_, private24BitBlock2, _ := net.ParseCIDR("127.0.0.0/8")
+	_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
+	_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
+	private = private24BitBlock2.Contains(IP) || private24BitBlock.Contains(IP) || private20BitBlock.Contains(IP) || private16BitBlock.Contains(IP)
+
 	return private
 
 }
--- src/common/utils_test.go
+++ src/common/utils_test.go
@@ -12,6 +12,12 @@ type testRecord struct {
 	output bool
 }
 
+type testIPRecord struct {
+	input  string
+	input2 bool
+	output bool
+}
+
 func TestCheckMoniker(t *testing.T) {
 
 	var tests = []testRecord{
@@ -35,6 +41,29 @@ func TestCheckMoniker(t *testing.T) {
 				test.input, strconv.FormatBool(ret), strconv.FormatBool(test.output))
 		} else {
 			t.Logf("%s => %s", test.input, strconv.FormatBool(test.output))
+		}
+	}
+}
+
+func TestCheckIP(t *testing.T) {
+
+	var tests = []testIPRecord{
+		testIPRecord{input: "127.0.0.1", input2: false, output: true},
+		testIPRecord{input: "10.0.0.1:1337", input2: false, output: true},
+		testIPRecord{input: "127.0.0.1", input2: true, output: true},
+		testIPRecord{input: ":8080", input2: false, output: true},
+		testIPRecord{input: ":8080", input2: true, output: false},
+		testIPRecord{input: "8.8.8.8", input2: true, output: false},
+		testIPRecord{input: "192.168.1.12", input2: true, output: true},
+	}
+
+	for _, test := range tests {
+		ret := common.CheckIP(test.input, test.input2)
+		if ret != test.output {
+			t.Errorf("\nWrong Answer: %s\nGot: %s\nExpected: %s\n",
+				test.input, strconv.FormatBool(ret), strconv.FormatBool(test.output))
+		} else {
+			t.Logf("%s %t => %s", test.input, test.input2, strconv.FormatBool(test.output))
 		}
 	}
 }
