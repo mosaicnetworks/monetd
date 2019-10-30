cdbce93790d5c11fbf190d87e83a571e7a3f0576
Jon Knight <jon@babble.io>
2019-10-30 13:06:14 +0000

    Working rebuild test

--- e2e/tests/rebuild/run-test.sh
+++ e2e/tests/rebuild/run-test.sh
@@ -5,60 +5,168 @@ set -eu
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 NET=${1:-"rebuild"}
+NETDIR=$HOME/.giverny/networks/$NET
+TMP_DIR=/tmp/rebuild.$$
+HOST="172.77.5.10"
+PORT="8080"
+ACCTS=10
+TRANS=100
 
+# Requires monetcli version 1.2.6 or later
+
+mkdir -p $TMP_DIR
 
 echo "Generate some transaction history"
 echo ""
 
-$mydir/../../tools/build-trans.sh -v --accounts=10 --transactions=100 --faucet="Faucet" \
-  --prefix=Test --node-name=Node --node-host=172.77.5.10 --node-port=8080 \
-  --config-dir=$HOME/.monettest --temp-dir=/tmp --faucet-config-dir=$HOME/.giverny/networks/$NET/keystore
+$mydir/../../tools/build-trans.sh -v --accounts=$ACCTS --transactions=$TRANS --faucet="Faucet" \
+  --prefix=Test --node-name=Node --node-host=$HOST --node-port=$PORT \
+  --config-dir=$HOME/.monettest --temp-dir=/tmp --faucet-config-dir=$NETDIR/keystore
 
 ex=$?
 if [ $ex -ne 0 ] ; then
     exit $ex
 fi
 
-# Sleep to allow the blocks to be committed.
-sleep 2
+addr0=$(sed -e 's/",.*$//g;s/^.*":"//g'  $NETDIR/keystore/node0.json)
+addr1=$(sed -e 's/",.*$//g;s/^.*":"//g'  $NETDIR/keystore/node1.json)
+addr2=$(sed -e 's/",.*$//g;s/^.*":"//g'  $NETDIR/keystore/node2.json)
+addr3=$(sed -e 's/",.*$//g;s/^.*":"//g'  $NETDIR/keystore/node3.json)
+addr4="6666666666666666666666666666666666666666" # Dummy address for an incomplete nomination
 
-$mydir/../../scripts/testlastblock.sh $( giverny network dump $NET | awk -F "|" '{print $2}')
+echo -e "\nVoting\n======\n\n"
 
-ex=$?
-if [ $ex -ne 0 ] ; then
-    exit $ex
-fi
+# Nominate node 3
+monetcli --datadir $NETDIR poa nominee new --pwd $NETDIR/keystore/node3.txt --moniker node3 --from node3 \
+     -h $HOST -p $PORT $addr3
+
+# Nominate dummy node 4
+monetcli --datadir $NETDIR poa nominee new --pwd $NETDIR/keystore/node0.txt --moniker node4 --from node0 \
+     -h $HOST -p $PORT $addr4   # Node 0 nominates our dummy node 4 as we have not generated a key
+
+# Node 0 votes for node 3
+monetcli --datadir $NETDIR poa nominee vote --pwd $NETDIR/keystore/node0.txt  --from node0 \
+     -h $HOST -p $PORT --verdict true $addr3 -d
+
+# Node 1 votes for node 3
+monetcli --datadir $NETDIR poa nominee vote --pwd $NETDIR/keystore/node1.txt  --from node1 \
+     -h $HOST -p $PORT --verdict true $addr3
+
+# Node 2 votes for node 3
+monetcli --datadir $NETDIR poa nominee vote --pwd $NETDIR/keystore/node2.txt  --from node2 \
+     -h $HOST -p $PORT --verdict true $addr3
+
+# Node 3 should be decided here
+
+# Node 2 votes for node 4
+monetcli --datadir $NETDIR poa nominee vote --pwd $NETDIR/keystore/node2.txt  --from node2 \
+     -h $HOST -p $PORT --verdict true $addr4
 
 
-# Authorise node 3
+# Pause to allow join blocks to be committed.
+sleep 1
 
 echo "Starting node 3"
 
-docker start node3
+giverny network push $NET node3
 
 # We allow node3 time to join and sync with the other nodes
 # We could replace this is a loop that monitors the completed rounds of all the 
 # node and break when they all level out.
 # But a simple sleep works too...
 
-# We start another election - just to verify the state carries over the reset
-
 sleep 5 
 
-# We get the current state from :8080/export
-# We also get the peers from :8080/peers
+$mydir/../../scripts/testlastblock.sh $( giverny network dump $NET | awk -F "|" '{print $2}')
 
-# We then write these files to the network configuration
+ex=$?
+if [ $ex -ne 0 ] ; then
+    exit $ex
+fi
+
+# We get the current state from :$PORT/export
+# We also get the peers from :$PORT/peers
+
+
+wget -O $TMP_DIR/genesis.json  http://$HOST:$PORT/export
+ex=$?
+if [ $ex -ne 0 ] ; then
+    exit $ex
+fi
+
+
+
+wget -O $TMP_DIR/peers.json  http://$HOST:$PORT/peers
+ex=$?
+if [ $ex -ne 0 ] ; then
+    exit $ex
+fi
+
 
 # Generate account balances
 
-# We destroy the current docker nodes
- 
-# We create a new network with the new config
+echo "Before Balances"
+for n in $(seq 1 $ACCTS)
+do
+  echo -n "."
+  ADDR=$(sed -e 's/",.*$//g;s/^.*":"//g'  $HOME/.monettest/keystore/Test$n.json)
+  wget -O $TMP_DIR/before$n.json  http://$HOST:$PORT/account/$ADDR
+done
+echo ""
 
-# We bring it up
+
+# Overwrite docker config
+
+find $NETDIR/docker -name genesis.json -exec cp $TMP_DIR/genesis.json {} \;
+find $NETDIR/docker -name peers.json -exec cp $TMP_DIR/peers.json {} \;
+find $NETDIR/docker -name peers.genesis.json -exec cp $TMP_DIR/peers.json {} \;
+
+
+echo -e "\nRestart\n=======\n\n"
+
+
+# Stop nodes
+
+docker stop node3
+docker stop node2
+docker stop node1
+docker stop node0
+
+# Destroy the node
+
+docker container prune -f
+
+# Running nodes:
+docker ps --all
+
+giverny network push $NET node0
+giverny network push $NET node1
+giverny network push $NET node2
+giverny network push $NET node3
+
+
+sleep 5
+
+ 
+
 
 # We compare the account totals that we generated earlier. 
+# Generate account balances
+
+echo "After Balances"
+for n in $(seq 1 $ACCTS)
+do
+#  echo "Test$n"
+  ADDR=$(sed -e 's/",.*$//g;s/^.*":"//g'  $HOME/.monettest/keystore/Test$n.json)
+  wget -O $TMP_DIR/after$n.json  http://$HOST:$PORT/account/$ADDR
+  cmp --silent $TMP_DIR/before$n.json $TMP_DIR/after$n.json || ( echo "Test $n  files are different" && exit 3)
+  
+done
+echo ""
 
 
-$mydir/../../scripts/testlastblock.sh $( giverny network dump $NET | awk -F "|" '{print $2}')
+# $mydir/../../scripts/testlastblock.sh $( giverny network dump $NET | awk -F "|" '{print $2}')
+
+
+#//TODO remove echo
+rm -rf $TMP_DIR
